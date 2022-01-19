291b4bb2bdfdd7b6fda1299f0d9ee27fb5ad78bb
hienhan0901 <hienhan0901@gmail.com>
2022-01-19 12:59:53 +0700

    fix

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,29 @@
 const router = require('express').Router()
 const usersModel = require('../models/user')
 
-router.get('/:username', async (req, res) => {
+router.get('/get-username/:id', async (req, res) => {
     try {
-        const users = await usersModel.find({
-            username: new RegExp(req.params.username, 'i')
+        const user = await usersModel.findOne({
+            _id: req.params.id
         })
 
-        res.status(200).json(users)
+        res.status(200).json(user.username)
     } catch (e) {
         res.status(500).json(e)
     }
 })
 
+router.get('/:username/:myName', async (req, res) => {
+    try {
+        const users = await usersModel.find({
+            username: new RegExp(req.params.username, 'i')
+        })
+
+        const users1 = users.filter(u => u.username !== req.params.myName)
+
+        res.status(200).json(users1)
+    } catch (e) {
+        res.status(500).json(e)
+    }
+})
 module.exports = router
